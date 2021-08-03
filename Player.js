aed0e0efdf941cb306e07e8e04e61fc1d8ce1479
pawanrana2006 <86482218+pawanrana2006@users.noreply.github.com>
2021-08-03 19:26:49 +0530

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,9 @@
+class Player{
+    // Write the code to add constructor. See hint in project document.
+
+
+
+    //Add a method move in the class Player.See hint
+	
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Crossy-Road--2
+# Crossy-Road-2---How-To-do
+How to do the Crossy Road 2 Project
